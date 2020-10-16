460f5c32165fb492128e6f4be26b74ac00159a07
Almu Iturgoyen <46594025+almu-itur@users.noreply.github.com>
2020-10-16 11:55:13 +0200

    node + express + eslint + prettier + some config

---
+++ src/server/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/new", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
